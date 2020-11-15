dcada703eb65ce91b6372bc7472e6ce4ad2bb1cd
Burak Köken <burakkokenn@gmail.com>
2020-11-15 22:31:17 +0200

    Update titles and project names in siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,16 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
+  title: 'Procyon', // Title for your website.
+  splashTitle: 'Procyon Web Framework', // Title for your website.
+  tagline: 'Build your application fast and deploy in Go lang',
   url: 'https://procyon-projects.github.io/', // Your website URL
-  baseUrl: '/docs/', // Base URL for your project */
+  baseUrl: '/procyon-documentation/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docs',
+  projectName: 'procyon-documentation',
   organizationName: 'procyon-projects',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
